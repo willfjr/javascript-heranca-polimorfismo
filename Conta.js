b592a3d2097270fcb02e567d70ce90ef7a7ccf84
wilson.ferreira <willferreirajr@icloud.com>
2022-04-13 08:38:32 -0300

    Implementa método e classe abstrata

---
+++ Conta.js
@@ -0,0 +1,54 @@
+export class Conta {
+
+    constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstata");
+        }
+        this._saldoInicial = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    //Método abstrato
+    sacar(valor) {
+        throw new Error("O método Sacar da conta é um método abstrato!")
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        }
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+
+    }
+}
